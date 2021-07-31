5b002e7ce4718e4d214c4a85e22a066dab00d179
CreamyMilk <creamym254@gmail.com>
2021-07-31 07:06:27 +0300

    Updated Tests

--- exchange/exchange_test.go
+++ exchange/exchange_test.go
@@ -18,9 +18,9 @@ func TestConvert(t *testing.T) {
 		{"NGN to GSH standard", "NGN", "GHS", 200, 2, true},
 		{"KSH to NGN decimals", "KSH", "NGN", 0.01, 0.03, true},
 		{"NGN to GHS decimals", "NGN", "GHS", 15.50, 0.155, true},
-		{"Negative Amount", "KSH", "GHS", 100, 0, false},
-		{"Zero Amount", "NGN", "KSH", 100, 0, false},
-		{"KSH to NONE exstant currency", "KSH", "UNKNOWN", 100, 100, false},
+		{"Negative Amount", "KSH", "GHS", -100, 0, false},
+		{"Zero Amount", "NGN", "KSH", 0, 0, false},
+		{"KSH to NONE existant currency", "KSH", "UNKNOWN", 100, 100, false},
 		{"NONE existant to existant currency", "UNKNOWN", "KSH", 100, 100, false},
 	}
 
